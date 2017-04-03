a2435cca75d0b57cadc355d3b3bde5f3c2e88be5
Sheyne Anderson <sheyne@sheyne.com>
2017-04-03 02:28:59 -0600

    bugfix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export { Plugin } from "./plugin";
-export { Model } from "./model"
-export { PluginLoaderManager } from "./plugin-loader";
-export { Program } from "./program";
+export * from "./plugin";
+export * from "./model"
+export * from "./plugin-loader";
+export * from "./program";
 export * from "./files";
